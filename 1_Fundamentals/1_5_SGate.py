308cb2fddfc7bbbdbe251fa5ee7a196b7bfd3362
main <tompecher.official@gmail.com>
2025-07-06 21:30:22 +0100

    Qiskit Fundamentals Section 5

--- 1_Fundamentals/1_5_SGate.py
+++ 1_Fundamentals/1_5_SGate.py
@@ -1,6 +1,6 @@
 
 # Qiskit Fundamentals
-# 1.4 Z-Gate
+# 1.5 S-Gate
 
 
 from qiskit import QuantumCircuit
@@ -8,17 +8,20 @@ from qiskit_aer import Aer
 from qiskit.visualization import plot_histogram
 import matplotlib.pyplot as plt
 
-# The Z (Pauli) gate is a quantum gate that flips the phase of a qubit.
-# Z = | 1 0  |
-#     | 0 -1 |
+# The S gate is a quantum gate that rotates the phase of a qubit by π/4 radians.
+# S = | 1 0 |
+#     | 0 i |
 
 qc = QuantumCircuit(1, 1)
 
 # Start in |0⟩, H puts it in |+⟩:
 qc.h(0)
 
-# Z flips phase of |+⟩, state becomes |-⟩:
-qc.z(0)
+# S rotates the phase of |+⟩, state becomes |i⟩:
+qc.s(0)
+
+# S again rotates the phase of |i⟩, state becomes |-⟩:
+qc.s(0)
 
 # Final H, outcome is |1⟩:
 qc.h(0)
@@ -30,6 +33,6 @@ result = simulator.run(qc, shots=1024).result()
 counts = result.get_counts()
 
 
-print("Z Gate Result:", counts)
-plot_histogram(counts, title="Z Gate on |+⟩")
+print("S Gate Result:", counts)
+plot_histogram(counts, title="S Gate on |+⟩")
 plt.show()
